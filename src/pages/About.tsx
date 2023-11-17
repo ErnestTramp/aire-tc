ec5cedc528df57631d9e604edfa086bf1e05859a
ErnestTramp <ernesttramp@gmail.com>
2023-11-17 13:10:26 +0000

    WIP About Us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,26 +11,20 @@ export default function About() {
         <div className="aboutHero">
           <div className="aboutHeroText">
             <div className="aboutHeroTextFirst">
-              <h1 className="community">
-                Community <IoIosArrowForward />
-              </h1>
-              <h1 className="driven">driven</h1>
-              <h1 className="progression">progression</h1>
-            </div>
-            <div className="aboutHeroTextSecond">
-              <p>
-                {
-                  "Aire Trampolining Club is a non-profit community organisation."
-                }
-              </p>
+              <p>Aire is about,</p>
+              <h1>Community driven progression.</h1>
             </div>
             <div className="aboutHeroTextBottomRow">
+              <div className="aboutHeroTextBottom1">
+                <p>
+                  As a club we are passionate about providing each gymnast the
+                  chance to participate in a safe and rewarding environment that
+                  emphasises enjoyment and skill learning.
+                </p>
+              </div>
               <div className="aboutHeroTextBottom">
-                <Link to="/">Meet us.</Link>
+                <Link to="/">Meet us</Link>
                 <div className="teamInfo">
-                  <p className="flex items-center gap-1">
-                    Our team <IoIosArrowForward className="translate-y-[10%]" />
-                  </p>
                   <div className="teamInfoImages">
                     <div className="teamImage1"></div>
                     <div className="teamImage2"></div>
@@ -39,16 +33,8 @@ export default function About() {
                   </div>
                 </div>
               </div>
-              <div className="aboutHeroTextBottom1">
-                <p>
-                  As a club we are passionate about providing each gymnast the
-                  chance to participate in a safe and rewarding environment that
-                  emphasises enjoyment and skill learning.
-                </p>
-              </div>
             </div>
           </div>
-          <div className="aboutHeroImage"></div>
         </div>
       </div>
       <Separator className="w-[70%]" />
